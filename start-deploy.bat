5e9989a9fc73b1c560766ff664fa52e23f6427e2
Yetkin Aslan <yetkinaslan7@gmail.com>
2025-04-14 15:52:02 +0300

    async_fonk

--- start-deploy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start cmd /k "cd /d .\client && npm run dev"
-start cmd /k "cd /d .\server && npm start"
-start cmd /k "cd /d .\ml-backend && call .\venv\Scripts\activate && cd pythonServer && daphne pythonServer.asgi:application"
-
